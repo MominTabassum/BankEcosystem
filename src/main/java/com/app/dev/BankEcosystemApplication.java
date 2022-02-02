4ea3ff18000694895abcca4f9eb57b554e359e32
MominTabassum <tabassummomin098@gmail.com>
2022-02-02 17:39:40 +0530

    first commit

---
+++ src/main/java/com/app/dev/BankEcosystemApplication.java
@@ -0,0 +1,16 @@
+package com.app.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@Configuration
+public class BankEcosystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankEcosystemApplication.class, args);
+		
+	}
+
+}
---
+++ src/test/java/com/example/BankEcosystem/BankEcosystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BankEcosystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankEcosystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
